08b5809e2852a78b4573512391e5a4f48b8491f1
Lindsay Lopez <lopez95@berkeley.edu>
2021-04-06 21:15:15 -0700

    added functionality to player buttons

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,11 +3,15 @@ const viewer = document.querySelector('.viewer');
 const toggleButton = document.querySelector('.toggle');
 const volumeSlider = document.querySelector('.volume');
 const playbackRateSlider = document.querySelector('.playback');
+const backwardButton = document.querySelector('.backward');
+const forwardButton = document.querySelector('.forward');
 
 viewer.addEventListener('click', togglePlay);
 toggleButton.addEventListener('click', togglePlay);
 volumeSlider.addEventListener('change', handleVolume);
 playbackRateSlider.addEventListener('change', handlePlaybackRate);
+backwardButton.addEventListener('click', handleBackwardButton);
+forwardButton.addEventListener('click', handleForwardButton);
 
 function togglePlay() {
   if (video.paused) {
@@ -25,4 +29,12 @@ function handleVolume() {
 
 function handlePlaybackRate() {
   video.playbackRate = playbackRateSlider.value;
+}
+
+function handleBackwardButton() {
+  video.currentTime += +backwardButton.dataset.skip;
+}
+
+function handleForwardButton() {
+  video.currentTime += +forwardButton.dataset.skip;
 }
