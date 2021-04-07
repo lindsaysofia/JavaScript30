03c91776ce4e09b0ad9d769ed0a4df553f3d2584
Lindsay Lopez <lopez95@berkeley.edu>
2021-04-06 20:44:58 -0700

    added functionality to volume slider

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,9 +1,11 @@
 const video = document.querySelector('video');
 const viewer = document.querySelector('.viewer');
 const toggleButton = document.querySelector('.toggle');
+const volumeSlider = document.querySelector('.volume');
 
 viewer.addEventListener('click', togglePlay);
 toggleButton.addEventListener('click', togglePlay);
+volumeSlider.addEventListener('change', handleVolume);
 
 function togglePlay() {
   if (video.paused) {
@@ -13,4 +15,8 @@ function togglePlay() {
     video.pause();
     toggleButton.innerHTML = '►';
   }
+}
+
+function handleVolume() {
+  video.volume = volumeSlider.value;
 }
