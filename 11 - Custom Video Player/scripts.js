4c63e7fe2aa2d3208a88383fd862ee2a47980092
Lindsay Lopez <lopez95@berkeley.edu>
2021-04-06 21:03:56 -0700

    added functionality to playback slider

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,10 +2,12 @@ const video = document.querySelector('video');
 const viewer = document.querySelector('.viewer');
 const toggleButton = document.querySelector('.toggle');
 const volumeSlider = document.querySelector('.volume');
+const playbackRateSlider = document.querySelector('.playback');
 
 viewer.addEventListener('click', togglePlay);
 toggleButton.addEventListener('click', togglePlay);
 volumeSlider.addEventListener('change', handleVolume);
+playbackRateSlider.addEventListener('change', handlePlaybackRate);
 
 function togglePlay() {
   if (video.paused) {
@@ -19,4 +21,8 @@ function togglePlay() {
 
 function handleVolume() {
   video.volume = volumeSlider.value;
+}
+
+function handlePlaybackRate() {
+  video.playbackRate = playbackRateSlider.value;
 }
