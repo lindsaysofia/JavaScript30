5fdbfd6ca5d2b961504e12896a77875aa17aa5b5
Lindsay Lopez <lopez95@berkeley.edu>
2021-05-10 22:06:56 -0700

    starting on using setInterval and date objects

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -2,12 +2,19 @@ const timerButtons = document.querySelectorAll('.timer__button');
 const customInput = document.querySelector('#custom');
 const displayTimeLeft = document.querySelector('.display__time-left');
 const displayEndTime = document.querySelector('.display__end-time');
+
+let timer;
 let timeLeft;
 let endTime;
 
 timerButtons.forEach(button => button.addEventListener('click', timer));
 
-function timer(e) {
-  
+function handleClick(e) {
+  clearInterval(timer);
+  timer = setInterval(() => {
+    let currentDate = new Date();
+    endTime = currentDate.getMilliseconds() + e.target.dataset.time;
+    console.log(endTime);
+  }, 1000);
 
 }
