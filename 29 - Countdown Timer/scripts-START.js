043d637a13f53aedcf470c0f5dfcab5401afd7d7
Lindsay Lopez <lopez95@berkeley.edu>
2021-05-11 17:06:01 -0700

    able to calc and display end time yay

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -4,17 +4,22 @@ const displayTimeLeft = document.querySelector('.display__time-left');
 const displayEndTime = document.querySelector('.display__end-time');
 
 let timer;
-let timeLeft;
+let secondsLeft;
 let endTime;
 
-timerButtons.forEach(button => button.addEventListener('click', timer));
+timerButtons.forEach(button => button.addEventListener('click',handleClick));
 
 function handleClick(e) {
   clearInterval(timer);
-  timer = setInterval(() => {
-    let currentDate = new Date();
-    endTime = currentDate.getMilliseconds() + e.target.dataset.time;
-    console.log(endTime);
-  }, 1000);
-
+  secondsLeft = e.target.dataset.time;
+  let currentTime = new Date();
+  endTime = new Date();
+  endTime.setMilliseconds(currentTime.getMilliseconds() + secondsLeft * 1000);
+  /* regex below per https://stackoverflow.com/questions/19407305/how-to-show-only-hours-and-minutes-from-javascript-date-tolocaletimestring */
+  displayEndTime.textContent = `Be back at ${endTime.toLocaleTimeString().replace(/:\d+ /, ' ')`};
+//   timer = setInterval(() => {
+//     console.log(timeLeft);
+    
+//     console.log(endTime.toLocaleTimeString());
+//   }, 1000);
 }
